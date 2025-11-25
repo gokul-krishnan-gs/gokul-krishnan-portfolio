63afdb04eb7af6788b6172040dfc0d3b3e834a76
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-11-25 17:35:26 +0530

    6: feat: theme toggle is added and working good . localstorage is provided.

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,51 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react"
+
+export default function ThemeToggle() {
+const [isDarkMode,setIsDarkMode] = useState(false)
+
+
+useEffect(()=>{
+
+
+    const storedTheme =  localStorage.getItem("theme");
+
+    if(storedTheme === 'dark'){
+        setIsDarkMode(true);
+        document.documentElement.classList.add("dark");  
+    }else{
+        document.documentElement.classList.remove("dark");
+        setIsDarkMode(false);     
+    }
+
+},[])
+
+function handleToggleTheme(){
+    if(isDarkMode){
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme","light");
+        setIsDarkMode(false);
+    }else{
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme","dark");
+        setIsDarkMode(true);
+    }
+}
+  return (
+    <button 
+        onClick={handleToggleTheme}
+        className="fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300"
+    >
+        {isDarkMode 
+            ? 
+            <Sun 
+                className="h-6 w-6 text-yellow-300"
+            /> 
+            : 
+            <Moon 
+                className="h-6 w-6 text-blue-900"
+            />
+        }
+    </button>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-
+import ThemeToggle from '../components/ThemeToggle'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+    </div>
   )
 }
