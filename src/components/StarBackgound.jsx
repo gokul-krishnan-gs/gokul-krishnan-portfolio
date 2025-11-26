d9e20e6662ae6510d0c8e56544767bde2941d383
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-11-26 08:15:39 +0530

    8: Meteors generation done and added the star backgorund to the home component

--- src/components/StarBackgound.jsx
+++ src/components/StarBackgound.jsx
@@ -3,10 +3,23 @@ import { useState } from "react"
 
 export default function StarBackgound() {
    const [stars,setStars] =  useState([]);
+   const [meteors,setMeteors] = useState([]);
+
+
 
    useEffect(()=>{
     generateStars();
+    generateMeteors();
+
+    function handleResize(){
+        generateStars();
+    }
+    
+    window.addEventListener("resize",handleResize);
+    return ()=>window.removeEventListener("resize",handleResize);
    },[]);
+
+
 
    function generateStars(){
     const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 10000);
@@ -26,6 +39,26 @@ export default function StarBackgound() {
 
     setStars(newStars);
    }
+
+   function generateMeteors(){
+    const numberOfMeteors = 4;
+
+    const newMeteors = [];
+
+    for(let i = 0;i< numberOfMeteors;i++){
+        newMeteors.push({
+            id: i,
+            size: Math.random() * 2 + 1,
+            x: Math.random() * 100,
+            y:Math.random() * 20,
+            delay: Math.random() * 15,
+            animationDuration: Math.random() * 3 + 3
+        });
+    }
+
+    setMeteors(newMeteors);
+   }
+
   return (
     <div
         className="fixed inset-0 overflow-hidden pointer-events-none z-0"
@@ -42,6 +75,24 @@ export default function StarBackgound() {
                         top:star.y + "%",
                         opacity: star.opacity,
                         animationDuration:star.animationDuration + "s",
+                    }}
+                > 
+
+                </div>
+            ))
+        }
+        {
+            meteors.map((meteor)=>(
+                <div 
+                    key={meteor.id}
+                    className="meteor animate-meteor"
+                    style={{
+                        width:meteor.size * 50 + "px",
+                        height:meteor.size * 1 + "px",
+                        left:meteor.x + "%",
+                        top:meteor.y + "%",
+                        animationDelay: meteor.delay + "s",
+                        animationDuration:meteor.animationDuration + "s",
                     }}
                 > 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import StarBackgound from '../components/StarBackgound'
 import ThemeToggle from '../components/ThemeToggle'
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    <div 
+      className="min-h-screen bg-background text-foreground overflow-x-hidden"
+    >
       <ThemeToggle />
+      <StarBackgound />
     </div>
   )
 }
